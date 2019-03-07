97b43b47dad92493135a6f8e941d498891ad5355
Antonio <ant.ccbezerra@gmail.com>
2019-03-07 12:30:33 +0000

    Added ex3_4

--- Ficha/3/main.cpp
+++ Ficha/3/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double round(double x, unsigned n) {
+    return floor(x * pow(10, n) + 0.5) / pow(10, n);
+}
+
+int ex3_4() {
+    double num;
+    int n;
+    cout << "Number: ";
+    cin >> num;
+    cout << "Decimal places: ";
+    cin >> n;
+    cout << setprecision(n) << fixed << round(num, n);
+    return 0;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
