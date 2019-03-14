b61f2fe07804f82f37294570f9e73d0d0ff49c13
Antonio <ant.ccbezerra@gmail.com>
2019-03-14 13:03:10 +0000

    Started 3.6

--- Ficha/3/main.cpp
+++ Ficha/3/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
+// ex3_4
 double round(double x, unsigned n) {
     return floor(x * pow(10, n) + 0.5) / pow(10, n);
 }
@@ -37,8 +39,8 @@ bool readFrac(int &numerator, int &denominator) {
         return true;
     else
         numerator = 0;
-        denominator = 0;
-        return false;
+    denominator = 0;
+    return false;
 }
 
 void writeFrac(int numerator, int denominator) {
@@ -62,27 +64,25 @@ void calcFrac(int num1, int den1, int num2, int den2, char op) {
             res_den = den1 * den2;
             break;
 
-        //Subtraction
+            //Subtraction
         case '-':
             res_num = num1 * den2 - num2 * den1;
             res_den = den1 * den2;
             break;
 
-        //Multiplication
+            //Multiplication
         case '*':
             res_num = num1 * num2;
             res_den = den1 * den2;
             break;
 
-        //Division
+            //Division
         case '/':
             res_num = num1 * den2;
             res_den = num2 * den1;
             break;
     }
-    cout << res_num << endl;
-    cout << res_den << endl;
-    writeFrac(res_num, res_den);
+
     reduceFrac(res_num, res_den);
     writeFrac(res_num, res_den);
 }
@@ -95,8 +95,85 @@ int ex3_5() {
 //    reduceFrac(num, den);
 //    writeFrac(num, den);
 
-    calcFrac(1,2,1,3,'+');
+    calcFrac(1, 2, 1, 3, '+');
 
     return 0;
 }
 
+// ex3_6
+bool leapYear(int year) {
+    if (year % 400 == 0)
+        return true;
+    else if (year % 100 == 0)
+        return false;
+    else if (year % 4 == 0)
+        return true;
+    else
+        return false;
+}
+
+int daysInMonth(int month, int year) {
+    if (month < 1 || month > 12)
+        return -1;
+
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8
+        || month == 10 || month == 12)
+        return 31;
+    else if (month == 2) {
+        if (leapYear(year))
+            return 29;
+        else
+            return 28;
+    } else
+        return 30;
+}
+
+int weekDay(int year, int month, int day) {
+    int wday, s, a, c;
+    int month_code[12][2] = {
+            {0, 6},
+            {3, 2},
+            {3, 3},
+            {6, 6},
+            {1, 1},
+            {4, 4},
+            {6, 6},
+            {2, 2},
+            {5, 5},
+            {0, 0},
+            {3, 3},
+            {5, 5}
+    };
+
+    s = year / 100;
+    a = year - s * 100;
+    c = month_code[month - 1][leapYear(year)];
+
+    wday = (int(floor(5 * a / 4)) + c + day - 2 * (s % 4) + 7) % 7;
+
+    return wday;
+}
+
+void monthCalendar(int month, int year) {
+    string months[12] = {"January", "February", "March",
+                         "April", "May", "June", "July",
+                         "August", "September", "October",
+                         "November", "December"};
+    int space = 8;
+
+// Formats the header of the calendar
+    cout << months[month - 1] << "/" << year << endl;
+    cout << "Sun" << setw(space) << "Mon" << setw(space) << "Tue" << setw(space) << "Wed"
+    << setw(space) << "Thu" << setw(space) << "Fri" << setw(space) << "Sat" << endl;
+
+// Writes the first month of the calendar with necessary white space
+    for (int i = (weekDay(year, month, 1) - 1) % 7; i >= 0; i--) {
+        cout << "   " << setw(space);
+    }
+    cout << 1;
+
+}
+
+int main() {
+    monthCalendar(3, 2019);
+}
